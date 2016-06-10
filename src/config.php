9399917e2dd4f08f9ad8452d12ab73d8f0a3aba8
Joel E. Svensson <joeles_93@hotmail.com>
2016-06-10 13:13:00 +0200

    First test exploratory version of PrivateApi

--- src/config.php
+++ src/config.php
@@ -14,5 +14,13 @@ return [
      * These config variables are optional
      */
     'DEFERRED_RESULT_MAX_ATTEMPTS'  => env('BSD_TOOLS_DEFERRED_RESULT_MAX_ATTEMPTS', 20),
-    'DEFERRED_RESULT_INTERVAL'      => env('BSD_TOOLS_DEFERRED_RESULT_INTERVAL', 5)
+    'DEFERRED_RESULT_INTERVAL'      => env('BSD_TOOLS_DEFERRED_RESULT_INTERVAL', 5),
+
+    /**
+     * For the machine API
+     */
+    'PRIVATE_API_URL'           => env('BSD_TOOLS_PRIVATE_API_URL', null),
+    'PRIVATE_API_EMAIL'         => env('BSD_TOOLS_PRIVATE_API_EMAIL', null),
+    'PRIVATE_API_PASSWORD'      => env('BSD_TOOLS_PRIVATE_API_PASSWORD', null),
+    'PRIVATE_API_SESSION_ID'    => env('BSD_TOOLS_PRIVATE_API_SESSION_ID', null),
 ];
